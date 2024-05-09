e31f5c1210db56c6f58337152f31f11685ef33f3
Robert Richardson <robert@peakzebra.com>
2024-05-09 12:55:46 -0400

    bump version for multi-product

--- pzvideocore.php
+++ pzvideocore.php
@@ -5,7 +5,7 @@
  *                    PZ plugins.
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           0.1.3
+ * Version:           0.1.4
  * Author:            Robert Richardson
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
@@ -14,6 +14,11 @@
  * @package           pzcore
  *
  */
+
+ /**
+  * 0.1.4 -- bumped version for multi-product
+  */
+  
 if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 /**
